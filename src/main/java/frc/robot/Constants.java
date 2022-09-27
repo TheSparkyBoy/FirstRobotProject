60bbabb534f556f033da515e3806a3060ad78795
Alex Nie <93626002+TheSparkyBoy@users.noreply.github.com>
2022-09-27 15:01:36 -0700

    Teaching 1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,22 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    //motor id's
+    public static final int FrontRightID = 0;
+    public static final int FrontLeftID = 3;
+    public static final int BackRightID = 5;
+    public static final int BackLeftID = 2;
+
+    //controller joystick id's
+    public static final int XBOX_R_XAXIS = 4;
+    public static final int XBOX_R_YAXIS = 5;
+    public static final int XBOX_L_XAXIS = 0;
+    public static final int XBOX_L_YAXIS = 1;
+    public static final int XBOX_LT = 2;
+    public static final int XBOX_RT = 3;
+
+    //controller pin
+    public static final int XBOX1_Pin = 0;
+    public static final int XBOX2_Pin = 1;
+}
